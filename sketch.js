8ce08245329c62afcabe715d0bbb4bb3bafa2e11
shreya <sarupula9@gmail.com>
2020-12-16 14:49:55 +0530

    Add Platform

--- sketch.js
+++ sketch.js
@@ -17,7 +17,8 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+     shreya
+     ddddddddd
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
